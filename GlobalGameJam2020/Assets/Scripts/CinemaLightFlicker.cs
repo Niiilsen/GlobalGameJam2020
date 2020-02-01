3f4835a46849d625e0056e71bb36e01a07403570
Niiilsen <nilsen.thomas@users.noreply.github.com>
2020-02-01 22:30:30 +0100

    Added portal and adjusted lights

---
+++ GlobalGameJam2020/Assets/Scripts/CinemaLightFlicker.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Light))]
+public class CinemaLightFlicker : MonoBehaviour
+{
+    Light myLight;
+    float x, y;
+    [SerializeField] private Vector2 flickerSpeed;
+
+    [SerializeField] private float minIntensity = 0;
+    [SerializeField] private float maxIntensity = 1;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myLight = GetComponent<Light>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        x += Time.deltaTime * flickerSpeed.x;
+        y += Time.deltaTime * flickerSpeed.y;
+        float randomNumber = Mathf.PerlinNoise(x, y);
+
+        randomNumber = Utils.Remap(randomNumber, 0,1, minIntensity, maxIntensity);
+        myLight.intensity = randomNumber;
+    }
+}
